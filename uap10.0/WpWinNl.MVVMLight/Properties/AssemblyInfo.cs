49ccddc1d39c2f56314c9f99d6b4ea5cfc478079
Joost van Schaik <joostvanschaik@outlook.com>
2015-10-31 12:03:55 +0100

    Added empty projects AGAIN

--- uap10.0/WpWinNl.MVVMLight/Properties/AssemblyInfo.cs
+++ uap10.0/WpWinNl.MVVMLight/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpWinNl.MVVMLight")]
+[assembly: AssemblyTitle("WpWinNl.MvvmLight")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpWinNl.MVVMLight")]
+[assembly: AssemblyProduct("WpWinNl.MvvmLight")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ uap10.0/WpWinNl.MvvmLight/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpWinNl.MvvmLight
+{
+    public class Class1
+    {
+    }
+}
