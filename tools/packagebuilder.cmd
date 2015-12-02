9de1583b02d7d7910842dec565e4f84c4f77c0b0
Joost van Schaik <joostvanschaik@outlook.com>
2015-12-02 16:46:03 +0100

    Updated nuspec to create the complete packages, as well fixed

--- tools/packagebuilder.cmd
+++ tools/packagebuilder.cmd
@@ -10,6 +10,11 @@ del /S/Q ..\output\Microsoft.Xaml.*
 del /S/Q ..\output\System.Windows.Interactivity.*
 del /S/Q ..\output\IBehaviorP.*
 
+del /S/Q "..\output\wpwinnl\lib\uap10.0\WpWinNl.dll"
+del /S/Q "..\output\wpwinnl\lib\uap10.0\WpWinNl.pri"
+del /S/Q "..\output\wpwinnl\lib\uap10.0\WpWinNl.External.dll"
+del /S/Q "..\output\wpwinnl\lib\uap10.0\WpWinNl.External.pri"
+
 del /S/Q "..\output\wpwinnl\lib\portable-win81+wpa81\WpWinNl.dll"
 del /S/Q "..\output\wpwinnl\lib\portable-win81+wpa81\WpWinNl.pri"
 del /S/Q "..\output\wpwinnl\lib\portable-win81+wpa81\WpWinNl.External.dll"
@@ -26,15 +31,21 @@ del /S/Q "..\output\wpwinnl\lib\WindowsPhone8\WpWinNl.External.dll"
 del /S/Q "..\output\wpwinnl\lib\WindowsPhone8\WpWinNl.External.pri"
 
 
+del /S/Q "..\output\wpwinnl_maps\lib\uap10.0\WpWinNl.dll"
+del /S/Q "..\output\wpwinnl_maps\lib\uap10.0\WpWinNl.pri"
+del /S/Q "..\output\wpwinnl_maps\lib\uap10.0\WpWinNl.External.dll"
+del /S/Q "..\output\wpwinnl_maps\lib\uap10.0\WpWinNl.External.pri"
+
 del /S/Q "..\output\wpwinnl_maps\lib\wpa81\WpWinNl.dll"
 del /S/Q "..\output\wpwinnl_maps\lib\wpa81\WpWinNl.pri"
 del /S/Q "..\output\wpwinnl_maps\lib\wpa81\WpWinNl.External.dll"
 del /S/Q "..\output\wpwinnl_maps\lib\wpa81\WpWinNl.External.pri"
 
 
+
 mkdir ..\output\wpwinnl_basic\tools
 copy *.ps1 ..\output\wpwinnl_basic\tools
 
 Nuget pack ..\SolutionInfo\WpWinNl.nuspec -BasePath ..\output\wpwinnl -OutputDirectory ..\output 
-nuget\Nuget pack ..\SolutionInfo\WpWinNlBasic.nuspec -BasePath ..\output\wpwinnl_basic -OutputDirectory ..\output 
-nuget\Nuget pack ..\SolutionInfo\WpWinNlMaps.nuspec -BasePath ..\output\wpwinnl_maps -OutputDirectory ..\output 
+Nuget pack ..\SolutionInfo\WpWinNlBasic.nuspec -BasePath ..\output\wpwinnl_basic -OutputDirectory ..\output 
+Nuget pack ..\SolutionInfo\WpWinNlMaps.nuspec -BasePath ..\output\wpwinnl_maps -OutputDirectory ..\output 
