0ba6e5efe2e7befe87d5c9c19e777604e7b640e4
Joost van Schaik <joostvanschaik@outlook.com>
2015-12-11 17:37:31 +0100

    Maps package inadvertendely had a depencendy on MVVMLigt. Removed and updated maps package version number

--- SolutionInfo/SolutionInfo.cs
+++ SolutionInfo/SolutionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2013, 2014, 2015 Joost van Schaik")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
 [assembly: AssemblyProduct("WpWinNl")]
