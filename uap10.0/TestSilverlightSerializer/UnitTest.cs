f1265c37ba466f6c41d48e1b54a0d10a496e69e9
Joost van Schaik <joost.van.schaik@wortell.nl>
2016-02-20 22:41:24 +0100

    Added some unit tests

--- uap10.0/TestSilverlightSerializer/UnitTest.cs
+++ uap10.0/TestSilverlightSerializer/UnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Windows.Devices.Geolocation;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using WpWinNl.Utilities;
@@ -18,5 +19,61 @@ namespace TestSilverlightSerializer
         deserialized.Position.Latitude == p.Position.Latitude);
     }
 
+
+    [TestMethod]
+    public void TestNullBoolean1()
+    {
+      bool? p = null;
+      var serialized = SilverlightSerializer.Serialize(p);
+      var deserialized = SilverlightSerializer.Deserialize(serialized) as bool?;
+      Assert.IsNull(deserialized);
+    }
+
+    [TestMethod]
+    public void TestNullBoolean2()
+    {
+      bool? p = false;
+      var serialized = SilverlightSerializer.Serialize(p);
+      var deserialized = SilverlightSerializer.Deserialize(serialized) as bool?;
+      Assert.IsFalse(deserialized.Value);
+    }
+
+
+    [TestMethod]
+    public void TestNullBoolean3()
+    {
+      bool? p = true;
+      var serialized = SilverlightSerializer.Serialize(p);
+      var deserialized = SilverlightSerializer.Deserialize(serialized) as bool?;
+      Assert.IsTrue(deserialized.Value);
+    }
+
+    [TestMethod]
+    public void TestDoNotSerialize()
+    {
+      var p  = new TestSerializable {  ToSerialize = "serialize this", ToSkip = "skip this"};
+      var serialized = SilverlightSerializer.Serialize(p);
+      var deserialized = SilverlightSerializer.Deserialize(serialized) as TestSerializable;
+      Assert.AreEqual(p.ToSerialize, deserialized.ToSerialize);
+      Assert.IsNull(deserialized.ToSkip);
+    }
+
+
+    [TestMethod]
+    public void TestGetAttributeNotToSerialize()
+    {
+      var p = new TestSerializable { ToSerialize = "serialize this", ToSkip = "skip this" };
+      var propInfo = p.GetType().GetRuntimeProperty("ToSkip");
+      Assert.IsNotNull(propInfo.GetCustomAttribute<DoNotSerialize>());
+    }
+
+    [TestMethod]
+    public void TestGetAttributeToSerialize()
+    {
+      var p = new TestSerializable { ToSerialize = "serialize this", ToSkip = "skip this" };
+      var propInfo = p.GetType().GetRuntimeProperty("ToSerialize");
+      Assert.IsNull(propInfo.GetCustomAttribute<DoNotSerialize>());
+    }
   }
+
 }
