5e8f0edeb526ef4c82eee9eb6a96f0cee921eedb
Joost van Schaik <joostvanschaik@outlook.com>
2016-03-23 14:56:17 +0100

    Made WpWinNl.Maps forward compatible EventToHandlerMapper -> EventToHandlerMapper

---
+++ wpa81/WpWinNl.Maps/EventToHandlerMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace WpWinNl.Maps
+{
+  /// <summary>
+  /// This class solely exists to make code shareable between Wp8.1 and Windows 10.
+  /// </summary>
+  public class EventToHandlerMapper : EventToCommandMapper
+  {
+  }
+}
